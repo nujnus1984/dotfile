2a49819654c5e22daf0cb32696b5f3440e74e989
nujnus <nujnus9221@163.com>
2015-05-25 22:58:47 +0800

    first commit

---
+++ git_config.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+git config --global user.email "nujnus9221@163.com"
+git config --global user.name "nujnus"
+
---
+++ link_dot.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p ~/.tmuxinator
+for file in ./tmuxinator/*
+do
+  if [ -f $file ]
+  then
+    ln -t ~/.tmuxinator/ $file
+  fi
+done
+
+#单个文件的link,前一个是源, 后一个是目标
+ln  ./inputrc  ~/.inputrc
+ 
